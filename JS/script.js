cf87a2aa7b04bd2c17a676bc33fb6837d419a9e8
Roshni Pasapu <roshnipasapu@Roshnis-MacBook-Air.local>
2024-08-09 19:04:06 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,189 @@
+// Global Variables
+/* global moment from moment.js*/
+const apiKey = "8aeca2ffebc6962c43c0e96825f3d12b", // for openweathermap.org
+  units = "imperial", // parameter to return degress F from API
+  // from local storage...
+  currentCity = localStorage.getItem("current"); // city to display on load
+let cities = JSON.parse(localStorage.getItem("history"));
+if (!cities) {
+  cities = { history: [] };
+}
+
+// Functions
+function updateHistory() {
+  // updates the cities listed in sidebar
+  // first clear history before update to prevent duplicates
+  $("#history").empty();
+  cities.history.forEach((city) => {
+    // then create a new div with the city name name
+    const cityEl = $("<div>")
+      .attr("data-city", city)
+      .addClass("text-muted p-2 bg-white border city-history")
+      .text(city);
+    // add delete button
+    cityEl.append(
+      $("<span>")
+        .addClass(
+          "text-danger px-1 float-right border border-danger rounded city-delete"
+        )
+        .html("&times;")
+    );
+    // and add it to the history ID
+    $("#history").append(cityEl);
+  });
+  // save cities to localstorage
+  localStorage.setItem("history", JSON.stringify(cities));
+}
+
+function deleteCity(city) {
+  const delIndex = cities.history.indexOf(city);
+  cities.history.splice(delIndex, 1);
+  updateHistory();
+}
+
+function getWeather(city) {
+  const currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`; // current weather api from openweathermap.org
+
+  // openweathermap.org Current Weather API
+  $.ajax({
+    url: currentURL,
+    method: "GET",
+  }).then(function (response) {
+    const lat = response.coord.lat, // latitude for One Call API
+      lon = response.coord.lon, // longitude for One Call API
+      parts = "minutely,hourly", // parts to exclude in One Call API
+      oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&exclude=${parts}&appid=${apiKey}`,
+      todaysDate = moment.unix(response.dt).format("MM/DD/YYYY"),
+      currentIconURL = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`,
+      currentIconAlt = response.weather[0].description;
+    let cityName = response.name;
+
+    if (city.includes(",")) {
+      const stateCountry = city.split(",");
+      for (let i = 1; i < stateCountry.length; i++) {
+        cityName += `, ${stateCountry[i].trim().toUpperCase()}`;
+      }
+    }
+    // display city name, date, & weather icon in main section header
+    $("#city-name").text(`${cityName} (${todaysDate})`);
+    $("#city-name").append(
+      $("<img>")
+        .attr("src", currentIconURL)
+        .attr("alt", currentIconAlt)
+        .attr("width", 75)
+    );
+
+    // add city to local storage & search history
+    localStorage.setItem("current", cityName);
+    if (!cities.history.includes(cityName)) {
+      // if cityName not in cities.history..
+      cities.history.unshift(cityName);
+    }
+    updateHistory(); // update list of cities on sidebar
+
+    // openweathermap.org One Call API
+    $.ajax({
+      url: oneCallURL,
+      method: "GET",
+    }).then(function (response) {
+      // update city current weather data
+      $("#current-temp").text(response.current.temp.toFixed(1));
+      $("#current-humid").text(response.current.humidity);
+      $("#wind").text(response.current.wind_speed);
+      $("#UV").text(response.current.uvi);
+      // color-code UV index
+      if (response.current.uvi <= 5) {
+        // UV is low
+        $("#UV").removeClass("bg-danger bg-warning").addClass("bg-success");
+      } else if (response.current.uvi >= 8) {
+        // UV is high
+        $("#UV").removeClass("bg-success bg-warning").addClass("bg-danger");
+      } else {
+        // UV is middle
+        $("#UV").removeClass("bg-danger bg-success").addClass("bg-warning");
+      }
+
+      // update 5-day forecast for city
+      $("#forecast").empty(); // removes currently displayed 5-day forcast
+      for (let i = 1; i <= 5; i++) {
+        // start at 1 because 5 day forecast starts with tomorrow
+        // get daily data from response
+        const data = response.daily[i],
+          date = moment.unix(data.dt).format("MM/DD/YY"),
+          iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+          iconAlt = data.weather[0].description,
+          temp = `Temp: ${data.temp.day.toFixed(1)} &deg;F`,
+          humid = `Humidity: ${data.humidity}%`,
+          // display that data on forecast card
+          cardEl = $("<div>").addClass(
+            "card bg-primary text-light p-2 m-2 mx-md-auto"
+          );
+
+        // add data to the forecast card
+        cardEl.append($("<p>").addClass("card-text text-center").text(date));
+        cardEl.append(
+          $("<img>")
+            .attr("src", iconURL)
+            .attr("alt", iconAlt)
+            .attr("width", 50)
+            .addClass("mx-auto d-block")
+        );
+        cardEl.append(
+          $("<p>").addClass("card-text text-center small").html(temp)
+        );
+        cardEl.append(
+          $("<p>").addClass("card-text text-center small").text(humid)
+        );
+
+        // add forecast card to 5-day forecast div
+        $("#forecast").append(cardEl);
+      }
+    });
+  });
+}
+
+// Main
+// load last viewed city from local storage
+if (currentCity) {
+  getWeather(currentCity);
+} else {
+  getWeather("Raleigh, NC, US");
+}
+// show helpful toast
+$(document).ready(function () {
+  $(".toast").toast("show");
+});
+
+// add event listeners
+// search button event listener
+$("#search").click(function (event) {
+  event.preventDefault();
+  // get user input
+  const cityForm = $("#city").val().trim(),
+    stateForm = $("#state").val().trim(),
+    countryForm = $("#country").val().trim();
+  let search4city = cityForm;
+
+  if (stateForm) {
+    search4city = `${cityForm}, ${stateForm}, US`;
+  } else if (countryForm) {
+    search4city = `${cityForm}, ${countryForm}`;
+  }
+
+  getWeather(search4city);
+  // clear search fields
+  $("#city").val("");
+  $("#state").val("");
+  $("#country").val("");
+});
+
+// search history event listener
+$("#history").on("click", ".city-history", function () {
+  getWeather($(this).attr("data-city"));
+});
+
+// search history delete event listener
+$("#history").on("click", ".city-delete", function (event) {
+  event.stopPropagation();
+  deleteCity($(this).parent().attr("data-city"));
+});
---
+++ README.md
@@ -0,0 +1,41 @@
+# [Weather Dashboard](https://thadkingcole.github.io/weather_dashboard)
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/35092138d7f24ef8af317d4a8b4c72a6)](https://app.codacy.com/manual/thadkingcole/weather_dashboard?utm_source=github.com&utm_medium=referral&utm_content=thadkingcole/weather_dashboard&utm_campaign=Badge_Grade_Dashboard) ![GitHub top language](https://img.shields.io/github/languages/top/thadkingcole/weather_dashboard)
+
+A simple weather dashboard that shows the user the weather outlook for multiple cities. Using [OpenWeather API](https://openweathermap.org/api), the user can find the weather for their desired location. The locations searched are saved for easy reviewing, and can be removed as desired by the user.
+
+[![main page](./public/images/screenshot.png)](https://thadkingcole.github.io/weather_dashboard)
+
+## Usage
+
+### Getting Started
+
+First, navigate to <https://thadkingcole.github.io/weather_dashboard>
+
+If this is your first time visiting the page, the weather for Raleigh, NC will be displayed. If Raleigh's weather is not of interest to you, you can [search for a new city](#searching-for-a-new-city).
+
+### Searching for a New City
+
+To find the weather of a new city, simply type in that city into the search bar and hit enter or press the blue search button.
+
+![search](./public/images/02_search.png)
+
+You can help narrow the search by including the 2 letter state abbreviation (if applicable) and/or the 2 letter country abbreviation. This is especially useful to distinguish between two cities in different states that share the same name such as Albany, NY and Albany, GA.
+
+### Search History
+
+With each city you find, it will be added to your search history directly below the search bar on the left side of the page.
+
+![history](./public/images/03_history.png)
+
+To view the weather for any of the cities in your search history, simply click on that city. If you wish to remove a city from your history, click the red &times; to the right of the city you wish to remove.
+
+![history demo](./public/images/04_history_demo.gif)
+
+## Credits
+
+Thanks to [OpenWeather](https://openweathermap.org/) and their [suite of APIs](https://openweathermap.org/api).
+
+## License
+
+[GPL 3.0](LICENSE)
